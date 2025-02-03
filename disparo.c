0bd0eb84b23102d7037dd64081da526b9a3b36e6
luizzrosario <luizzrosario@hotmail.com>
2025-02-03 11:38:37 -0300

    base

--- disparo.c
+++ disparo.c
@@ -1,40 +1,87 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+#define BUTTON_PIN 5
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+volatile bool button_pressed = false;
+volatile bool sequence_running = false;
 
+// Função de callback para o botão (com debounce)
+bool debounce_button() {
+    static uint32_t last_press_time = 0;
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+    if (current_time - last_press_time > 200) { // 200ms de debounce
+        last_press_time = current_time;
+        return true;
+    }
+    return false;
+}
 
+void button_callback(uint gpio, uint32_t events) {
+    if (gpio == BUTTON_PIN && events == GPIO_IRQ_EDGE_FALL) {
+        if (!sequence_running && debounce_button()) {
+            button_pressed = true;
+        }
+    }
+}
 
-int main()
-{
+// Função de callback para o timer
+int64_t led_timer_callback(alarm_id_t id, void *user_data) {
+    static int led_state = 0;
+    switch (led_state) {
+        case 0:
+            gpio_put(LED_GREEN, 1);  // Liga LED GREEN
+            gpio_put(LED_BLUE, 1);   // Liga LED BLUE
+            gpio_put(LED_RED, 1);    // Liga LED RED
+            break;
+        case 1:
+            gpio_put(LED_RED, 0);    // Desliga LED RED
+            break;
+        case 2:
+            gpio_put(LED_BLUE, 0);   // Desliga LED BLUE
+            break;
+        case 3:
+            gpio_put(LED_GREEN, 0);  // Desliga LED GREEN
+            sequence_running = false; // Finaliza a sequência
+            break;
+    }
+    led_state++;
+    if (led_state < 4) {
+        add_alarm_in_ms(3000, led_timer_callback, NULL, false); // Próximo estado após 3 segundos
+    }
+    return 0;
+}
+
+int main() {
     stdio_init_all();
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
     
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_callback);
     
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_put(LED_GREEN, 0);
     
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_put(LED_BLUE, 0);
+    
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_put(LED_RED, 0);
+    
+    while (1) {
+        if (button_pressed && !sequence_running) {
+            button_pressed = false;     // Reseta a variável de controle do botão
+            sequence_running = true;    // Marca que a sequência começou
+            add_alarm_in_ms(3000, led_timer_callback, NULL, false); // Inicia a sequência de LEDs
+        }
+        sleep_ms(10);  // Aguarda um curto período de tempo antes de verificar novamente
     }
 }
